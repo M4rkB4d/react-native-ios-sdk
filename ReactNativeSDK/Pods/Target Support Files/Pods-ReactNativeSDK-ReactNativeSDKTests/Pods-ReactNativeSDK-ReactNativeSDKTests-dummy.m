7d10c8a41cbc300d84440ee1489ade73bb5aa20c
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2024-06-17 21:47:22 +0800

    add dependencies

---
+++ ReactNativeSDK/Pods/Target Support Files/Pods-ReactNativeSDK-ReactNativeSDKTests/Pods-ReactNativeSDK-ReactNativeSDKTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeSDK_ReactNativeSDKTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeSDK_ReactNativeSDKTests
+@end
