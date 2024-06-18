b1dd537bc2b6cfe96a1abb821cf509fd1f4e9b46
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2024-06-19 03:11:08 +0800

    remove generated files and update gitignore

--- ReactNativeSDK/Pods/Target Support Files/Pods-ReactNativeSDK-ReactNativeSDKTests/Pods-ReactNativeSDK-ReactNativeSDKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeSDK_ReactNativeSDKTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeSDK_ReactNativeSDKTests
-@end
--- ReactNativeSDK/Pods/Target Support Files/Pods-ReactNativeSDK/Pods-ReactNativeSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeSDK : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeSDK
-@end
